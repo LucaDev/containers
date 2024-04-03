f4cea46de5c4e18171430b65a2ea7825d752a46f
Luca Kröger <l.kroeger01@gmail.com>
2024-04-03 13:32:16 +0200

    fix bw-cli vault unlock

--- apps/bw-cli/entrypoint.sh
+++ apps/bw-cli/entrypoint.sh
@@ -11,9 +11,6 @@ if  ! /opt/bw login --check; then
     if [ ! -z ${BW_CLIENTID} ]; then
         echo "Using APIKEY Login"
         /opt/bw login --apikey
-        echo "Login successful. Unlocking Vault"
-        export BW_SESSION=$(/opt/bw unlock --passwordenv BW_PASSWORD --raw)
-        echo "Vault session acquired"
     else
         echo "Using Password Login"
         export BW_SESSION=$(/opt/bw login ${BW_USER} --passwordenv BW_PASSWORD --raw)
@@ -23,9 +20,8 @@ else
     echo "Already logged in."
 fi
 
-echo "Checking Vault lock"
-/opt/bw unlock --check
-echo "Vault unlock OK"
+echo "Unlocking Vault"
+export BW_SESSION=$(/opt/bw unlock --passwordenv BW_PASSWORD --raw)
 
 echo 'Running `bw server` on port 8087'
 exec /opt/bw serve --hostname all --disable-origin-protection $@
