4444803f9d1ccdf496c52169330efb5d4f84a238
Luca Kröger <l.kroeger01@gmail.com>
2024-04-03 10:38:57 +0200

    samba: add nslcd

---
+++ apps/samba/conf/nslcd.conf.tpl
@@ -0,0 +1,6 @@
+uri ${LDAP_URI}
+base ${LDAP_BASE}
+binddn ${LDAP_BINDDN}
+bindpw ${LDAP_BINDPW}
+uid nslcd
+gid nslcd
--- apps/samba/entrypoint.sh
+++ apps/samba/entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 
 cat /etc/smb.d/* > /etc/samba/smb.conf
 
+cat /etc/nslcd.conf.tpl | envsubst >> /etc/nslcd.conf 
+nslcd
+
 exec /usr/sbin/smbd -i
