e090921974821ce00185ad69107ebae33ce46bc4
Luca Kröger <l.kroeger01@gmail.com>
2024-04-04 13:41:43 +0200

    add jellyfin

---
+++ apps/jellyfin/ci/latest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+version="$(curl -sX GET "https://api.github.com/repos/jellyfin/jellyfin/releases" | jq -r 'map(select(.tag_name)))[0].tag_name' 2>/dev/null)"
+version="${version#*v}"
+printf "%s" "${version}"
