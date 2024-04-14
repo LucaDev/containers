70005aff512ed6c76f3a4ccecace234a8cb39cc5
Luca Kröger <l.kroeger01@gmail.com>
2024-04-14 09:57:38 +0200

    samba: fix versioning

--- apps/samba/ci/latest.sh
+++ apps/samba/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version=$(curl -s "https://sources.debian.org/api/src/samba/" | jq '.versions[] | select(.suites[] | contains("bookworm-backports")).version')
+version=$(curl -s "https://sources.debian.org/api/src/samba/" | jq '.versions[] | select(.suites[] | contains("bookworm-backports")).version' | sed 's/\"2://' | sed 's/+.*//')
 printf "%s" "${version}"
