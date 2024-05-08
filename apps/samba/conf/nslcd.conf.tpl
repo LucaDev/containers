cf84348814449f54541283f953d2467bd11478c9
Luca Kröger <l.kroeger01@gmail.com>
2024-05-08 23:05:18 +0200

    remove samba & wsdd2 containers

--- apps/samba/conf/nslcd.conf.tpl
+++
@@ -1,7 +0,0 @@
-uri ${LDAP_URI}
-base ${LDAP_BASE}
-binddn ${LDAP_BINDDN}
-bindpw ${LDAP_BINDPW}
-tls_reqcert allow
-uid nslcd
-gid nslcd
--- apps/samba/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-cat /etc/nslcd.conf.tpl | envsubst > /etc/nslcd.conf 
-nslcd
-
-cat /etc/smb.d/* > /etc/samba/smb.conf
-exec /usr/sbin/smbd -F --no-process-group
