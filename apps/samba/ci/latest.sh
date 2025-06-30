82c5eaad5d8f6b58c29728c1e356edde6bab00db
Luca <l.kroeger01@gmail.com>
2025-06-30 07:47:07 +0000

    samba: remove

--- apps/samba/ci/latest.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-echo -n $(curl -s "https://sources.debian.org/api/src/samba/" | jq '.versions[] | select(.suites[] | contains("bookworm-backports")).version' | sed 's/\"2://' | sed 's/+.*//' | head -n 1)
